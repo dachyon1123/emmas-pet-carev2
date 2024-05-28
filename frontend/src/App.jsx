ed496b3f2a7711ccc26333e56086efe387e9d5ad
Dylan Polito <dpolito1123@gmail.com>
2024-05-27 23:07:57 -0500

    set up vite and tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <p className='bg-slate-800 text-white text-[20px] py-1 text-right pr-2'>+1 956 708 3099</p>
+    </>
+  )
+}
+
+export default App
